0f709109d0b3669e10ea5693a6d30ac87a7f4c3a
cellul4r <seriousme.com@gmail.com>
2025-06-25 21:12:21 +0700

    override doGet method to send response back, load currency options in html file

--- src/main/java/com/cellul4r/currencyconverter/CurrencyConverter.java
+++ src/main/java/com/cellul4r/currencyconverter/CurrencyConverter.java
@@ -1,9 +1,15 @@
 package com.cellul4r.currencyconverter;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "currencyConverter", value = "/index")
+import java.io.IOException;
+import java.util.Arrays;
+
+@WebServlet(name = "currencyConverter", value = "/currency-converter")
 public class CurrencyConverter extends HttpServlet {
 
     private static final String[] EXCHANGE_RATEs_STR = {"AUD-AUD", "AUD-CNY", "AUD-EUR", "AUD-JPY", "AUD-SGD", "AUD-THB", "AUD-VND",
@@ -31,6 +37,25 @@ public class CurrencyConverter extends HttpServlet {
     private double targetCurrencyAmount;
 
     public void init() {
+    }
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String type = req.getParameter("type");
+        // check type of request
+        if("list".equals(type)) {
+
+            // format currency string array to json
+            String[] currencyList = Arrays.copyOf(this.currencyList, this.currencyList.length);
+            for(int i = 0; i < currencyList.length; i++) {
+                currencyList[i] = String.format("\"%s\"", currencyList[i]);
+            }
+            String currencysString = Arrays.toString(currencyList);
+
+            // set up response to send back
+            resp.setCharacterEncoding("UTF-8");
+            resp.setContentType("application/json");
+            resp.getWriter().write(currencysString);
+        }
     }
 }
