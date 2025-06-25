c380f1504c7931814fa094fd6ca27b68269b6336
cellul4r <seriousme.com@gmail.com>
2025-06-25 17:52:19 +0700

    Create CurrencyConverter Project and CurrencyConverter Class

---
+++ src/main/java/com/cellul4r/currencyconverter/CurrencyConverter.java
@@ -0,0 +1,9 @@
+package com.cellul4r.currencyconverter;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "currencyConverter", value = "/index")
+public class CurrencyConverter extends HttpServlet {
+
+}
