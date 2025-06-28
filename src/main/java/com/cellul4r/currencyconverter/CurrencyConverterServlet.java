7bb9f9baa0ec89979ee18c600880b9f39857c568
cellul4r <seriousme.com@gmail.com>
2025-06-28 16:56:53 +0700

    implement doPost method to send back to json to show conversion result on webpage

--- src/main/java/com/cellul4r/currencyconverter/CurrencyConverterServlet.java
+++ src/main/java/com/cellul4r/currencyconverter/CurrencyConverterServlet.java
@@ -1,16 +1,20 @@
 package com.cellul4r.currencyconverter;
 
+import com.google.gson.Gson;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.Arrays;
 
 @WebServlet(name = "currencyConverter", value = "/currency-converter")
 public class CurrencyConverterServlet extends HttpServlet {
+
+    private final Gson gson = new Gson();
 
     public void init() {
     }
@@ -38,6 +42,16 @@ public class CurrencyConverterServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader reader = req.getReader();
 
+        CurrencyConverter currencyConverter = gson.fromJson(reader, CurrencyConverter.class);
+        currencyConverter.convert();
+
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("application/json");
+
+        String json = gson.toJson(currencyConverter);
+        System.out.println(json);
+        resp.getWriter().write(json);
     }
 }
